edb3ecc665285ce77a14668c512be00cb1578291
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-01-27 09:57:59 +0100

    Desktop & Mobile Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.scss';
-import {ColorScheme, MantineProvider} from "@mantine/core";
-import {useLocalStorage} from "@mantine/hooks";
+import {MantineProvider} from "@mantine/core";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Body from "./components/Body/Body";
 import Header from "./components/Header/Header";
 
 export default function App() {
-
-    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-        key: 'mantine-color-scheme',
-        defaultValue: "light",
-        getInitialValueInEffect: true,
-    });
-
     return (
         <MantineProvider
             withGlobalStyles
             withNormalizeCSS
-            theme={{
-                colorScheme,
-                loader: 'bars',
-            }}
         >
             <div className="App">
                 <Header />
--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -1,20 +1,71 @@
-import {AiOutlineClose} from "@react-icons/all-files/ai/AiOutlineClose";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {HiBars3} from "react-icons/hi2";
+import {GrClose} from "@react-icons/all-files/gr/GrClose";
+import {SimpleGrid} from "@mantine/core";
+import Logo from "./Logo";
+import LogoName from "./LogoName";
 
 export default function Menu() {
     const [show, setShow] = useState(false);
 
     function toggle() {
         setShow(!show);
-        console.log(show);
     }
 
     if (show) {
         return <MobileMenu toggle={toggle}/>;
     }
 
-    return <OpenButton toggle={toggle}/>
+    if (window.innerWidth < 1100) {
+        return <OpenButton toggle={toggle}/>
+    } else {
+        return <DesktopMenu />
+    }
+
+}
+
+function DesktopMenu() {
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        const onScroll = () => setOffset(window.pageYOffset);
+        window.removeEventListener('scroll', onScroll);
+        window.addEventListener('scroll', onScroll, { passive: true });
+        // @ts-ignore
+        let header = document.getElementById("DesktopHeader");
+        if (offset > 0) {
+            // @ts-ignore
+            header.classList.add("Header-fade-in");
+            // @ts-ignore
+            header.classList.remove("Header-fade-out");
+        }
+        if (offset === 0) {
+            // @ts-ignore
+            header.classList.add("Header-fade-out");
+            // @ts-ignore
+            header.classList.remove("Header-fade-in");
+        }
+        return () => window.removeEventListener('scroll', onScroll);
+    }, [offset]);
+
+    return (
+        <div className={"DesktopHeader"} id={"DesktopHeader"}>
+            <SimpleGrid cols={2} >
+                <SimpleGrid cols={2} >
+                    <Logo />
+                    <LogoName />
+                </SimpleGrid>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Training</a></li>
+                    <li><a href="#">Vorstand</a></li>
+                    <li><a href="#">Weitere Informationen</a></li>
+                </ul>
+            </SimpleGrid>
+
+
+        </div>
+    );
 }
 
 function OpenButton(props: any) {
@@ -26,7 +77,7 @@ function OpenButton(props: any) {
 function MobileMenu(props: any) {
     return (
         <div className={"Menu"} id="toggle" >
-            <AiOutlineClose size={50}  className={"toggle"} onClick={props.toggle}/>
+            <GrClose size={50}  className={"toggle"} onClick={props.toggle}/>
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Training</a></li>
