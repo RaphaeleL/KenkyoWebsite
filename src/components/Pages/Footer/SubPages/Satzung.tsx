004ef98dcd47c061a2064f081c555b140734cac9
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-01-31 08:55:39 +0100

    Forgot to remove it

--- src/components/Pages/Footer/SubPages/Satzung.tsx
+++ src/components/Pages/Footer/SubPages/Satzung.tsx
@@ -1,7 +1,7 @@
 export default function Satzung(props: any) {
     return (
         <a href={ "https://raw.githubusercontent.com/RaphaeleL/KenkyoWebsite/main/src/assets/Satzung.pdf" } >
-            {props.title} (wip)
+            {props.title}
         </a>
     );
 }
