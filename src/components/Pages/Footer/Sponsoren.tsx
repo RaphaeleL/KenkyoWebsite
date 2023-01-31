5baf53898832017226a5d4c83b37a39507a7235f
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-01-31 08:25:20 +0100

    Sponsor / Spenden Page

--- src/components/Pages/Footer/Sponsoren.tsx
+++ src/components/Pages/Footer/Sponsoren.tsx
@@ -1,7 +1,11 @@
+import MyContent from "../../Utils/MyContent";
+
 export default function Sponsoren() {
     return (
         <>
-            TODO
+            <MyContent content={"Möchten Sie uns etwas Spenden oder uns Sponsoren? Dann melden Sie sich bei uns!"} />
+            <MyContent content={<a href={"mailto: todo@todo.de"}>todo@todo.de</a>} />
+            <MyContent content={"Wir freuen uns auf Ihre Nachricht. Doch nun die bisherigen Sponsoren und Spender:"} />
         </>
     );
 }
