25c4a49e61e58bf1d8c371c3822c29afd4a78c40
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2023-01-31 08:21:11 +0100

    Show the Elements without Scrolling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,7 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import MenuItems from "./MenuItems";
 
 export default function Header() {
-    const [offset, setOffset] = useState(0);
-
-    useEffect(() => {
-        const onScroll = () => setOffset(window.pageYOffset);
-        window.removeEventListener('scroll', onScroll);
-        window.addEventListener('scroll', onScroll, { passive: true });
-        // @ts-ignore
-        let header = document.getElementById("DesktopHeader");
-        if (offset > 0) {
-            // @ts-ignore
-            header.classList.add("Header-fade-in");
-            // @ts-ignore
-            header.classList.remove("Header-fade-out");
-        }
-        if (offset === 0) {
-            // @ts-ignore
-            header.classList.add("Header-fade-out");
-            // @ts-ignore
-            header.classList.remove("Header-fade-in");
-        }
-        return () => window.removeEventListener('scroll', onScroll);
-    }, [offset]);
 
     return (
         <div className={"DesktopHeader"} id={"DesktopHeader"}>
